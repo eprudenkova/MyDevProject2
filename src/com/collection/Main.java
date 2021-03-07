d8be071e06ce06b450525fbf660258ec57f4940e
eprudenkova <eprudenkova@yahoo.com>
2021-03-07 11:04:01 -0500

    first commit

---
+++ src/com/collection/Main.java
@@ -0,0 +1,13 @@
+package com.collection;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i=1; i<6;i++){
+            System.out.println("Testing");
+        }
+    }
+}
